6924c3887ef6a6f4a3818de34a5553266d338e7e
CodingASP <78342170+CodingASP@users.noreply.github.com>
2021-07-26 16:49:29 -0700

    7/26

--- src/_04_spot_the_error/MethodCall.java
+++ src/_04_spot_the_error/MethodCall.java
@@ -7,10 +7,11 @@ public class MethodCall {
 	// Add ONE line of code to fix this program.
 	
 	public static void main(String[] args) {
-		
+		System.out.print("Hello World");
 	}
-	String aMessage = ("Hello World");
+	
 	static void printMessage(String aMessage) {
-		System.out.println(aMessage);
+		System.out.print(aMessage);
+		
 	}
 }
--- src/_99_extra/NasaCountdown.java
+++ src/_99_extra/NasaCountdown.java
@@ -1,7 +1,9 @@
 
 package _99_extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown or the rocket won't launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -13,15 +15,42 @@ package _99_extra;
 public class NasaCountdown {
 	public static void main(String[] args) throws InterruptedException {
 		// 2. Use a pop-up to ask the user where to start counting from
-
+String SNumbers = JOptionPane.showInputDialog(null, "Pick a number 1-10 to start the countdown from.");
+int number = Integer.parseInt(SNumbers);
 		// 3. Change the countdown to use the new starting point
-		
-		// 1. Print a countdown from 10 to 0 on the console
+if (number == 10) {
+	System.out.println("10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0");
+} else if (number == 9) {
+	System.out.println("9, 8, 7, 6, 5, 4, 3, 2, 1, 0");
+} else if (number == 8) {
+	System.out.println("8, 7, 6, 5, 4, 3, 2, 1, 0");
+} else if (number == 7) {
+	System.out.println("7, 6, 5, 4, 3, 2, 1, 0");
+} else if (number == 6) {
+	System.out.println("6, 5, 4, 3, 2, 1, 0");
+} else if (number == 5) {
+	System.out.println("5, 4, 3, 2, 1, 0");
+} else if (number == 4 ){
+	System.out.println("4, 3, 2, 1, 0");
+} else if (number == 3 ){
+	System.out.println("3, 2, 1, 0");
+} else if (number == 2 ){
+	System.out.println("2, 1, 0");
+} else if (number == 1 ){
+	System.out.println("1, 0");
+} else if (number == 0 ){
+	System.out.println("0");
 	
+}
+		// 1. Print a countdown from 10 to 0 on the console
+
+
+	
+
 			// 4. Use the speak method to hear the countdown.
-		
+
 			// 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
-		
+		speak(SNumbers);
 		// 5. when the counting is done, speak "blastoff!"
 	}
 
